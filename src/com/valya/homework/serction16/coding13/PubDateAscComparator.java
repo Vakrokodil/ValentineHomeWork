ce5d55ca6311f9e7190c0178f239010e3197d9f2
Valentine <Vakrokodil@gmail.com>
2022-11-20 14:42:18 +0100

    coding 13

---
+++ src/com/valya/homework/serction16/coding13/PubDateAscComparator.java
@@ -0,0 +1,18 @@
+package com.valya.homework.serction16.coding13;
+
+import java.util.Comparator;
+
+public class PubDateAscComparator implements Comparator<Book> {
+
+    @Override
+    public int compare(Book o1, Book o2) {
+        if (o1.getYear() > o2.getYear()) {
+            return 1;
+        } else if (o1.getYear() < o2.getYear()) {
+            return -1;
+        } else {
+            return o1.compareTo(o2);
+        }
+    }
+}
+
